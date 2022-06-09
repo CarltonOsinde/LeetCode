f2cb96a8bcc4d88b54147987d6ad13f151b3f9d4
Carlton Osinde <47374849+CarltonOsinde@users.noreply.github.com>
2022-06-08 22:55:14 -0400

    Road to Google

---
+++ DS Basics/Arrarys.py
@@ -0,0 +1,23 @@
+#ARRAYS are also known as Lists []
+###
+
+#Search, Insert, Delete O(n) -Average & Worst Case
+# Access of O(1) - Average & Worst Case
+
+# ##
+
+
+
+
+#SLICING
+a = [1, 2, 3, 4, 5,6,7,8, 9, 10, 11, 12, 13, 14]
+print(a[:3])
+print(a[5:])
+print(a[5:9])
+
+
+#NOTE: Best to iterate through using a for loop & enumurate to showcase the index and element
+num = [5,10,15,20,25]
+for i, num in enumerate(num):
+    print(i,num)
+
---
+++ DS Basics/Stack..py
@@ -0,0 +1,40 @@
+#STACKS
+#NOTE: There is no pre-built function for Stacks so we use a List and initiate by defining it as a stack
+
+#lAST IN, FIRST OUT (LIFO)
+
+
+
+stack = []
+
+stack.append(3)
+stack.append(6)
+stack.append(9)
+stack.append(12)
+
+print(stack)
+
+
+print(stack[-3])
+
+stack.pop()
+print(stack)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
